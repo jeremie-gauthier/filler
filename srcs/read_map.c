f2d3d64521f244299d2f3c81b979fd29ba6e15e2
Jeremie <jeremiegthr@gmail.com>
2020-06-20 15:12:28 +0200

    fix leaks

--- srcs/read_map.c
+++ srcs/read_map.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 12:17:50 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/16 15:27:41 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/06/20 15:12:03 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static int read_map_header(char *line, t_filler *filler)
 {
   t_split data;
 
-  ft_bzero(&data, sizeof(t_split));
+  ft_bzero((void *)&data, sizeof(t_split));
   if (!(data.array = ft_strsplit(line, WHITESPACES, &data.len)))
     return (-1);
   filler->map->height = ft_atoi(data.array[1]);
@@ -32,24 +32,30 @@ static int read_map_body(t_filler *filler)
 
   i = 0;
   ft_bzero(&data, sizeof(t_split));
-  while (i <= filler->map->height && get_next_line(0, &data.str) > 0)
+  while (i < filler->map->height && get_next_line(0, &data.str) > 0)
   {
     ft_dprintf(2, "++ %s WITH LEN = %lu AT %i\n", data.str, data.len, i);
     if (!(data.array = ft_strsplit(data.str, WHITESPACES, &data.len)))
     {
+      ft_tabdel((void **)filler->map->data, i);
       split_clear(&data);
       return (-1);
     }
     if (data.len < 2)
+    {
+      split_clear(&data);
       continue;
-    if (!(filler->map->data[i - 2] = ft_strdup(data.array[1])))
+    }
+    if (!(filler->map->data[i] = ft_strdup(data.array[1])))
     {
       ft_tabdel((void **)filler->map->data, i);
+      split_clear(&data);
       return (-1);
     }
+    split_clear(&data);
     i++;
   }
-  filler->map->data[i - 2] = NULL;
+  filler->map->data[i] = NULL;
   return (0);
 }
 
@@ -57,9 +63,9 @@ int read_map(char *line, t_filler *filler)
 {
   if (read_map_header(line, filler) < 0)
     return (-1);
-  ft_dprintf(2, "::width => %lu || height => %lu ::\n",
+  ft_dprintf(2, ":: width => %lu || height => %lu ::\n",
              filler->map->width, filler->map->height);
-  if (map_init(filler) < 0)
+  if (data_init(&filler->map->data, filler->map->height) < 0)
     return (-1);
   if (read_map_body(filler) < 0)
     return (-1);
