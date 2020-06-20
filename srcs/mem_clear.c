c5627f9e1ec09e48c85e117d480674d7d5a7f139
Jeremie <jeremiegthr@gmail.com>
2020-06-20 15:11:41 +0200

    add clear mem fn for each struct

--- srcs/mem_clear.c
+++ srcs/mem_clear.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 13:07:51 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/16 13:09:34 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/06/20 11:51:41 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,36 @@
 void split_clear(t_split *split)
 {
   ft_strdel(&split->str);
-  ft_tabdel((void**)split->array, split->len);
+  ft_tabdel((void **)split->array, split->len);
+}
+
+void map_clear(t_map *map)
+{
+  ft_tabdel((void **)map->data, map->height);
+  ft_tabdel((void **)map->heatmap, map->height);
+}
+
+void piece_clear(t_piece *piece)
+{
+  ft_tabdel((void **)piece->data, piece->height);
+}
+
+void player_clear(t_player *player)
+{
+  ft_tabdel((void **)player->last_move, player->len);
 }
 
 void filler_clear(t_filler *filler)
 {
-    ft_memdel((void**)&filler->map);
-    ft_memdel((void**)&filler->me);
-    ft_memdel((void**)&filler->opponent);
+  map_clear(filler->map);
+  ft_memdel((void **)&filler->map);
+  piece_clear(filler->piece);
+  if (filler->piece)
+    ft_memdel((void **)&filler->piece);
+  // player_clear(filler->me);
+  if (filler->me)
+    ft_memdel((void **)&filler->me);
+  // player_clear(filler->opponent);
+  if (filler->opponent)
+    ft_memdel((void **)&filler->opponent);
 }
