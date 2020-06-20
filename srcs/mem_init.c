0e8677d0d28828ef83407d66ff914fc624dde2e4
Jeremie <jeremiegthr@gmail.com>
2020-06-20 19:09:03 +0200

    new method to init queue and heatmap

--- srcs/mem_init.c
+++ srcs/mem_init.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 13:10:21 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/20 12:04:51 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/06/20 18:53:25 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,39 @@ int data_init(char ***data, size_t size)
 {
   if (!(*data = (char **)ft_memalloc((size + 1) * sizeof(char *))))
     return (-1);
+  return (0);
+}
+
+int heatmap_init(unsigned int ***heatmap, size_t height, size_t width)
+{
+  size_t h;
+
+  h = 0;
+  if (!(*heatmap = (unsigned int **)ft_memalloc(height * sizeof(unsigned int *))))
+    return (-1);
+  while (h < height)
+  {
+    if (!((*heatmap)[h] = (unsigned int *)ft_memalloc(width * sizeof(unsigned int))))
+    {
+      ft_tabdel((void **)*heatmap, h);
+      return (-1);
+    }
+    h++;
+  }
+  return (0);
+}
+
+int queue_init(t_queue *queue, size_t size)
+{
+  if (!(queue->data = (t_coord *)ft_memalloc(size * sizeof(t_coord))))
+    return (-1);
+  if (!(queue->marked = (char *)ft_memalloc(size * sizeof(char))))
+  {
+    ft_memdel((void **)&queue->data);
+    return (-1);
+  }
+  queue->front = 0;
+  queue->rear = 0;
   return (0);
 }
 
