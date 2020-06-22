6ad998ee690c4479885b6350d5efe724656d3cfc
Jeremie <jeremiegthr@gmail.com>
2020-06-22 21:26:13 +0200

    working version

--- srcs/test_piece.c
+++ srcs/test_piece.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 18:21:42 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/22 20:47:23 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/06/22 21:17:01 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,21 +37,16 @@ int test_piece(t_coord coord, t_filler *filler)
   int w;
   int overlapping;
 
-  ft_dprintf(2, "TEST: [%i, %i]\n", coord.y, coord.x);
+  // ft_dprintf(2, "TEST: [%i, %i]\n", coord.y, coord.x);
   overlapping = 0;
   h = 0;
-  while ((h + filler->piece->offset_height) < filler->piece->height &&
-         (coord.y + h) < filler->map->height &&
-         !is_empty_row(filler->piece->data[h]))
+  while ((h + filler->piece->offset_height) < (filler->piece->offset_height + filler->piece->true_height))
   {
     w = 0;
-    while ((w + filler->piece->offset_width) < filler->piece->width &&
-           (coord.x + w) < filler->map->width &&
-           !is_empty_col(filler->piece->data, w))
+    while ((w + filler->piece->offset_width) < (filler->piece->offset_width + filler->piece->true_width))
     {
-      ft_dprintf(2, "CHECKING: [%i, %i]\n", coord.y + h, coord.x + w);
-      if (filler->map->data[coord.y + h]
-                           [coord.x + w] == '*' &&
+      if (is_player_pawn(filler->map->data[coord.y + h][coord.x + w],
+                         filler->me->shape) &&
           filler->piece->data[h + filler->piece->offset_height]
                              [w + filler->piece->offset_width] == '*')
         overlapping++;
@@ -67,22 +62,24 @@ int test_piece_moving_around(t_pawn target, t_filler *filler)
   t_coord coord;
   t_coord to_test;
 
+  // ft_dprintf(2, "TARGET: [%i, %i]\n", target.coord.y, target.coord.x);
   coord.y = 1;
   while (coord.y <= filler->piece->true_height)
   {
+    // ft_dprintf(2, "Is it to high ? %i < %i\n", (target.coord.y + filler->piece->true_height - coord.y), filler->map->height);
     if ((target.coord.y + filler->piece->true_height - coord.y) < filler->map->height &&
         (target.coord.y - coord.y + 1) >= 0)
     {
-      ft_dprintf(2, "PASS 1st condition\n");
+      // ft_dprintf(2, "PASS 1st condition\n");
       coord.x = 1;
       while (coord.x <= filler->piece->true_width)
       {
         if ((target.coord.x + filler->piece->true_width - coord.x) < filler->map->width &&
             (target.coord.x - coord.x + 1) >= 0)
         {
-          ft_dprintf(2, "PASS 2nd condition\n");
+          // ft_dprintf(2, "PASS 2nd condition\n");
           to_test.x = target.coord.x - coord.x + 1;
-          to_test.y = target.coord.y - coord.y + 2;
+          to_test.y = target.coord.y - coord.y + 1;
           if (test_piece(to_test, filler) == 1)
           {
             ft_dprintf(2, "{green}FOUND A PLACE @ [%i, %i]{reset}\n", to_test.y, to_test.x);
