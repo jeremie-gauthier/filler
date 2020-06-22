67c4bbeb7ec4eef1ecd32d8ec470f391e654aba0
Jeremie <jeremiegthr@gmail.com>
2020-06-22 11:31:37 +0200

    new method to get the offset of a piece

--- srcs/read_piece.c
+++ srcs/read_piece.c
@@ -6,56 +6,82 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 12:19:22 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/20 21:09:03 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/06/22 11:30:36 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-static int read_piece_header(char *line, t_filler *filler)
+static int read_piece_header(char *line, t_piece *piece)
 {
   t_split data;
 
   ft_bzero((void *)&data, sizeof(t_split));
   if (!(data.array = ft_strsplit(line, WHITESPACES, &data.len)))
     return (-1);
-  filler->piece->height = ft_atoi(data.array[1]);
-  filler->piece->width = ft_atoi(data.array[2]);
+  piece->height = ft_atoi(data.array[1]);
+  piece->width = ft_atoi(data.array[2]);
   ft_tabdel((void **)data.array, data.len);
   return (0);
 }
 
-static int read_piece_body(t_filler *filler)
+static int read_piece_body(t_piece *piece)
 {
   char *line;
   int i;
 
   i = 0;
   line = NULL;
-  while (i < filler->piece->height && get_next_line(0, &line) > 0)
+  while (i < piece->height && get_next_line(0, &line) > 0)
   {
     ft_dprintf(2, "++ %s WITH LEN = %lu AT %i\n", line, ft_strlen(line), i);
-    if (!(filler->piece->data[i] = line))
+    if (!(piece->data[i] = line))
     {
-      ft_tabdel((void **)filler->piece->data, i);
+      ft_tabdel((void **)piece->data, i);
       ft_strdel(&line);
       return (-1);
     }
     i++;
   }
-  filler->piece->data[i] = NULL;
+  piece->data[i] = NULL;
   return (0);
+}
+
+static void set_offset(t_piece *piece)
+{
+  int *offset_h;
+  int *offset_w;
+  int row;
+
+  offset_h = &piece->offset_height;
+  offset_w = &piece->offset_width;
+  while (!ft_strchr(piece->data[*offset_h], '*'))
+    (*offset_h)++;
+  while (*offset_w < piece->width)
+  {
+    row = 0;
+    while (row < piece->height)
+    {
+      if (piece->data[row][*offset_w] == '*')
+        return;
+      row++;
+    }
+    (*offset_w)++;
+  }
 }
 
 int read_piece(char *line, t_filler *filler)
 {
-  if (read_piece_header(line, filler) < 0)
+  if (read_piece_header(line, filler->piece) < 0)
     return (-1);
   ft_dprintf(2, ":: width => %lu || height => %lu ::\n",
              filler->piece->width, filler->piece->height);
   if (data_init(&filler->piece->data, filler->piece->height) < 0)
     return (-1);
-  if (read_piece_body(filler) < 0)
+  if (read_piece_body(filler->piece) < 0)
     return (-1);
+  set_offset(filler->piece);
+  ft_dprintf(2, "OFF Height: %i\nOFF Width: %i\n",
+             filler->piece->offset_height, filler->piece->offset_width);
   return (0);
 }
