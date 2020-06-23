d50bc32a966e39a23594defaede617a25918700f
Jeremie <jeremiegthr@gmail.com>
2020-06-23 12:19:07 +0200

    loop through all open pawns to find best spot to play

--- srcs/play_piece.c
+++ srcs/play_piece.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 12:20:03 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/22 20:29:45 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/06/23 12:14:05 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,42 @@ void print_pawns(t_player *me)
   }
 }
 
+static unsigned int select_piece_coords(t_filler *filler)
+{
+  size_t nb_visited;
+  unsigned int idx_pawn;
+  unsigned int ignore_lt;
+  int placeable;
+
+  placeable = 0;
+  ignore_lt = 0;
+  idx_pawn = 0;
+  nb_visited = 0;
+  while (!placeable && nb_visited < filler->me->len_pawns)
+  {
+    idx_pawn = find_pawn_with_min_dist(&filler->me->pawns[idx_pawn],
+                                       filler->me->len_pawns, ignore_lt);
+    ignore_lt = filler->me->pawns[idx_pawn].coord.dist;
+    filler->me->pawns[idx_pawn].tested = true;
+    nb_visited++;
+    if (idx_pawn < filler->me->len_pawns)
+    {
+      ft_dprintf(2, "{green}PAWN >> %i %i %lu{reset}\n",
+                 filler->me->pawns[idx_pawn].coord.y,
+                 filler->me->pawns[idx_pawn].coord.x,
+                 filler->me->pawns[idx_pawn].coord.dist);
+      placeable = test_piece_moving_around(&filler->me->pawns[idx_pawn], filler);
+    }
+    else
+    {
+      idx_pawn = 0;
+      ignore_lt++;
+    }
+  }
+  ft_dprintf(2, "PLACEABLE => %i\n", placeable);
+  return (placeable ? idx_pawn : ERROR_PAWN);
+}
+
 int play_piece(t_filler *filler)
 {
   unsigned int idx_pawn;
@@ -36,14 +72,14 @@ int play_piece(t_filler *filler)
     return (-1);
   pawns_counting(filler);
   print_pawns(filler->me);
-  idx_pawn = find_pawn_with_min_dist(filler->me->pawns, filler->me->len_pawns, 0);
-  ft_dprintf(2, "{green}PAWN >> %i %i %lu{reset}\n",
-             filler->me->pawns[idx_pawn].coord.y,
-             filler->me->pawns[idx_pawn].coord.x,
-             filler->me->pawns[idx_pawn].coord.dist);
-  // int res = test_piece(filler->me->pawns[idx_pawn].coord, filler);
-  int res = test_piece_moving_around(filler->me->pawns[idx_pawn], filler);
-  ft_dprintf(2, "PLACEABLE => %i\n", res);
+  if ((idx_pawn = select_piece_coords(filler)) == ERROR_PAWN)
+  {
+    player_clear(filler->me);
+    return (-1);
+  }
+  ft_printf("%i %i\n",
+            filler->me->pawns[idx_pawn].best_spot.x - filler->piece->offset_width,
+            filler->me->pawns[idx_pawn].best_spot.y - filler->piece->offset_height);
   player_clear(filler->me);
   return (0);
 }
