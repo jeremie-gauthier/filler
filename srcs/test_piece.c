5b9e8cbdb12d25e8814255995e4ba34e21d130bf
Jeremie <jeremiegthr@gmail.com>
2020-06-23 12:20:44 +0200

    change prototype to pass variable by reference instead by value

--- srcs/test_piece.c
+++ srcs/test_piece.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 18:21:42 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/22 21:17:01 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/06/23 12:13:40 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,32 +57,31 @@ int test_piece(t_coord coord, t_filler *filler)
   return (overlapping == 1);
 }
 
-int test_piece_moving_around(t_pawn target, t_filler *filler)
+int test_piece_moving_around(t_pawn *target, t_filler *filler)
 {
   t_coord coord;
-  t_coord to_test;
 
   // ft_dprintf(2, "TARGET: [%i, %i]\n", target.coord.y, target.coord.x);
   coord.y = 1;
   while (coord.y <= filler->piece->true_height)
   {
     // ft_dprintf(2, "Is it to high ? %i < %i\n", (target.coord.y + filler->piece->true_height - coord.y), filler->map->height);
-    if ((target.coord.y + filler->piece->true_height - coord.y) < filler->map->height &&
-        (target.coord.y - coord.y + 1) >= 0)
+    if ((target->coord.y + filler->piece->true_height - coord.y) < filler->map->height &&
+        (target->coord.y - coord.y + 1) >= 0)
     {
       // ft_dprintf(2, "PASS 1st condition\n");
       coord.x = 1;
       while (coord.x <= filler->piece->true_width)
       {
-        if ((target.coord.x + filler->piece->true_width - coord.x) < filler->map->width &&
-            (target.coord.x - coord.x + 1) >= 0)
+        if ((target->coord.x + filler->piece->true_width - coord.x) < filler->map->width &&
+            (target->coord.x - coord.x + 1) >= 0)
         {
           // ft_dprintf(2, "PASS 2nd condition\n");
-          to_test.x = target.coord.x - coord.x + 1;
-          to_test.y = target.coord.y - coord.y + 1;
-          if (test_piece(to_test, filler) == 1)
+          target->best_spot.x = target->coord.x - coord.x + 1;
+          target->best_spot.y = target->coord.y - coord.y + 1;
+          if (test_piece(target->best_spot, filler) == 1)
           {
-            ft_dprintf(2, "{green}FOUND A PLACE @ [%i, %i]{reset}\n", to_test.y, to_test.x);
+            ft_dprintf(2, "{green}FOUND A PLACE @ [%i, %i]{reset}\n", target->best_spot.y, target->best_spot.x);
             return (1);
           }
         }
