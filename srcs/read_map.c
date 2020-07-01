a09826aec6005071a76fca0af63ad81c2d6686e7
Jeremie <jeremiegthr@gmail.com>
2020-07-01 17:03:08 +0200

    norm n lines read_map

--- srcs/read_map.c
+++ srcs/read_map.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 12:17:50 by jergauth          #+#    #+#             */
-/*   Updated: 2020/06/29 11:08:17 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/07/01 17:02:41 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,13 @@ static int read_map_header(char *line, t_filler *filler)
   return (0);
 }
 
+static int clear_failure(char **map_data, int idx, t_split *split_data)
+{
+  ft_tabdel((void **)map_data, idx);
+  split_clear(split_data);
+  return (-1);
+}
+
 static int read_map_body(t_filler *filler)
 {
   t_split data;
@@ -35,22 +42,14 @@ static int read_map_body(t_filler *filler)
   while (i < filler->map->height && get_next_line(0, &data.str) > 0)
   {
     if (!(data.array = ft_strsplit(data.str, WHITESPACES, &data.len)))
-    {
-      ft_tabdel((void **)filler->map->data, i);
-      split_clear(&data);
-      return (-1);
-    }
+      return (clear_failure(filler->map->data, i, &data));
     if (data.len < 2)
     {
       split_clear(&data);
       continue;
     }
     if (!(filler->map->data[i] = ft_strdup(data.array[1])))
-    {
-      ft_tabdel((void **)filler->map->data, i);
-      split_clear(&data);
-      return (-1);
-    }
+      return (clear_failure(filler->map->data, i, &data));
     split_clear(&data);
     i++;
   }
