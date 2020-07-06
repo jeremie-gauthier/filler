a67d124315097cd477f2a54695bcacf5982c71ed
Jeremie <jeremiegthr@gmail.com>
2020-07-06 11:20:14 +0200

    add compiled version

---
+++ bonus/client/build/precache-manifest.3317a8918ba6b92827c9666fcdb466a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f46f40996950cf7ae79864a02e5a00e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ec71c5639b6c61d88af",
+    "url": "/static/css/main.882cdc3e.chunk.css"
+  },
+  {
+    "revision": "bc898f58105663919dca",
+    "url": "/static/js/2.41929c36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.41929c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec71c5639b6c61d88af",
+    "url": "/static/js/main.c9500a43.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
