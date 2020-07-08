9d53a62869f98136c048b43e08730772410c7f50
jeremie-gauthier <jeremiegthr@gmail.com>
2020-07-08 11:37:41 +0200

    norm

--- srcs/helpers.c
+++ srcs/helpers.c
@@ -6,27 +6,27 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 17:19:31 by jergauth          #+#    #+#             */
-/*   Updated: 2020/07/01 17:31:06 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/07/08 11:37:33 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-int is_empty_row(const char *row)
+int	is_empty_row(const char *row)
 {
-  return ft_strchr(row, '*') == NULL;
+	return (ft_strchr(row, '*') == NULL);
 }
 
-int is_empty_col(const char **array, const size_t idx_col)
+int	is_empty_col(const char **array, const size_t idx_col)
 {
-  size_t row;
+	size_t row;
 
-  row = 0;
-  while (array[row])
-  {
-    if (array[row][idx_col] == '*')
-      return (0);
-    row++;
-  }
-  return (1);
+	row = 0;
+	while (array[row])
+	{
+		if (array[row][idx_col] == '*')
+			return (0);
+		row++;
+	}
+	return (1);
 }
