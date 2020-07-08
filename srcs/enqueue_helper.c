dff882ab4a6b6a5494ea0e36e8ba3a6079b8a848
jeremie-gauthier <jeremiegthr@gmail.com>
2020-07-08 11:32:25 +0200

    norm

--- srcs/enqueue_helper.c
+++ srcs/enqueue_helper.c
@@ -6,28 +6,28 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 16:32:18 by jergauth          #+#    #+#             */
-/*   Updated: 2020/07/01 17:27:32 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/07/08 11:32:17 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-int is_in_queue(const t_coord coord, t_queue *queue)
+int	is_in_queue(const t_coord coord, t_queue *queue)
 {
-  size_t i;
+	size_t i;
 
-  i = queue->front;
-  while (i < queue->rear)
-  {
-    if (queue->data[i].x == coord.x && queue->data[i].y == coord.y)
-      return (1);
-    i++;
-  }
-  return (0);
+	i = queue->front;
+	while (i < queue->rear)
+	{
+		if (queue->data[i].x == coord.x && queue->data[i].y == coord.y)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int is_empty_case(t_filler *filler, const t_coord coord)
+int	is_empty_case(t_filler *filler, const t_coord coord)
 {
-  return filler->map->data[coord.y][coord.x] == EMPTY_CASE &&
-         filler->map->heatmap[coord.y][coord.x] == COLD_CASE;
+	return (filler->map->data[coord.y][coord.x] == EMPTY_CASE &&
+			filler->map->heatmap[coord.y][coord.x] == COLD_CASE);
 }
